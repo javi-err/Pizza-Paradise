bb5c6e79e4cae796346d289b51a979df05b19827
javi <jsmart1993@hotmail.com>
2020-11-17 09:58:20 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('.hamburger-menu').on('click', function (){
+        $('.toggle').toggleClass('open')
+        $('.hamburger-menu').css('background-color', 'white');
+    })
+})
